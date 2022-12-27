f22b8ea3dfc71d6fa4b60982bc44645faf3ab219
DylanVallejo <dylan1811vallejo@gmail.com>
2022-12-27 17:08:20 -0500

    agregando el componente clock html y css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Clock from './components/Clock';
 
 function App() {
   return (
     <div className="App">
       <h1>calculadora</h1>
+      <Clock/>
     </div>
   );
 }
